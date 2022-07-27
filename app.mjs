1224b8d8372b797213007b13256cefafeec971c4
Excel <>
2022-07-27 21:00:14 +0000

    added pointer cursor to submit btn

--- app.mjs
+++ app.mjs
@@ -6,17 +6,6 @@ function startApp() {
     // pls remove the below and make some magic in here!
 
 
-const sign_in_btn = document.querySelector('#sign-in-btn')
-const sign_up_btn = document.querySelector('#sign-up-btn')
-const container = document.querySelector('.container')
-
-sign_up_btn.addEventListener('click', () => {
-  container.classList.add('sign_up_mode')
-})
-
-sign_in_btn.addEventListener('click', () => {
-  container.classList.remove('sign_up_mode')
-})
 
   
   // ======= DO NOT EDIT ============== //
